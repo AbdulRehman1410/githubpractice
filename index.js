4eb1bdd2ceddf9a0c5488aa07dc1edd794f9cccf
AbdulRehman1410 <96471777+AbdulRehman1410@users.noreply.github.com>
2021-12-21 15:14:44 +0500

    first project

---
+++ index.js
@@ -0,0 +1,48 @@
+function red(){
+    document.getElementById('div2').style.backgroundColor = "red"
+    document.getElementById('div3').style.backgroundColor = "red"
+    document.getElementById('div4').style.backgroundColor = "red"
+    document.getElementById('div5').style.backgroundColor = "red"
+    document.getElementById('div6').style.backgroundColor = "red"
+    document.getElementById('div7').style.backgroundColor = "red"
+};
+function green(){
+    document.getElementById('div2').style.backgroundColor = "green"
+    document.getElementById('div3').style.backgroundColor = "green"
+    document.getElementById('div4').style.backgroundColor = "green"
+    document.getElementById('div5').style.backgroundColor = "green"
+    document.getElementById('div6').style.backgroundColor = "green"
+    document.getElementById('div7').style.backgroundColor = "green"
+};
+function yellow(){
+    document.getElementById('div2').style.backgroundColor = "yellow"
+    document.getElementById('div3').style.backgroundColor = "yellow"
+    document.getElementById('div4').style.backgroundColor = "yellow"
+    document.getElementById('div5').style.backgroundColor = "yellow"
+    document.getElementById('div6').style.backgroundColor = "yellow"
+    document.getElementById('div7').style.backgroundColor = "yellow"
+};
+function orange(){
+    document.getElementById('div2').style.backgroundColor = "orange"
+    document.getElementById('div3').style.backgroundColor = "orange"
+    document.getElementById('div4').style.backgroundColor = "orange"
+    document.getElementById('div5').style.backgroundColor = "orange"
+    document.getElementById('div6').style.backgroundColor = "orange"
+    document.getElementById('div7').style.backgroundColor = "orange"
+};
+function blue(){
+    document.getElementById('div2').style.backgroundColor = "blue"
+    document.getElementById('div3').style.backgroundColor = "blue"
+    document.getElementById('div4').style.backgroundColor = "blue"
+    document.getElementById('div5').style.backgroundColor = "blue"
+    document.getElementById('div6').style.backgroundColor = "blue"
+    document.getElementById('div7').style.backgroundColor = "blue"
+};
+function gray(){
+    document.getElementById('div2').style.backgroundColor = "gray"
+    document.getElementById('div3').style.backgroundColor = "gray"
+    document.getElementById('div4').style.backgroundColor = "gray"
+    document.getElementById('div5').style.backgroundColor = "gray"
+    document.getElementById('div6').style.backgroundColor = "gray"
+    document.getElementById('div7').style.backgroundColor = "gray"
+}
